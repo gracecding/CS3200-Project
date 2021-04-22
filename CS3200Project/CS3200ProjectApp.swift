e5e4d59f031b582b4af267516ba8297545a471a5
Grace Ding <graceding9@gmail.com>
2021-04-21 17:10:26 -0700

    Initial Commit

---
+++ CS3200Project/CS3200ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS3200ProjectApp.swift
+//  CS3200Project
+//
+//  Created by Grace Ding on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct CS3200ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
